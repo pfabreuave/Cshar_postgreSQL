aadca2e7f0743759a9071e6e7b33150f4513a8cd
Pedro Abreu Arenas <pfabreuave@gmail.com>
2022-04-29 10:55:49 -0300

    se modifica presentacion del SQL

--- projeto03/Form1.cs
+++ projeto03/Form1.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 /*
- * consultas pendientes, como corto el comando SQL en varias lineas
+ * pendiente agregar actualizacion del stock
  * como controlo el tamanho del comBox
  * como simplifico new
  * 
@@ -26,7 +26,6 @@ namespace projeto03
         private void Form1_Load(object sender, EventArgs e)
         {
             String strConex = "Server=localhost;Port=5432;Database=stone;User Id=postgres;Password=psap616201;";
-            //conex = new NpgsqlConnection();
             conex.ConnectionString = strConex;
             conex.Open();
             NpgsqlCommand cmd = new NpgsqlCommand();
@@ -42,22 +41,26 @@ namespace projeto03
 
         private void CboPolos_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string strSql = "SELECT polo, stock, venda, dias_hab, ceil(venda / dias_hab) as media, ceil(stock / (venda / dias_hab)) as auto, CASE WHEN(ceil(stock / (Venda / Dias_Hab))) < 10 THEN 1 WHEN(ceil(stock / (Venda / Dias_Hab))) >= 10 AND(ceil(stock / (Venda / Dias_Hab))) <= 13 THEN 2 WHEN(ceil(stock / (Venda / Dias_Hab))) >= 14 AND(ceil(stock / (Venda / Dias_Hab))) <= 18 THEN 3 WHEN(ceil(stock / (Venda / Dias_Hab))) >= 19 AND(ceil(stock / (Venda / Dias_Hab))) <= 23 THEN 4 ELSE 5 END AS cat, CASE WHEN(ceil(stock / (Venda / Dias_Hab))) >= 14 AND(ceil(stock / (Venda / Dias_Hab))) <= 18 THEN 0 ELSE(14 - (ceil(stock / (Venda / Dias_Hab)))) * ceil(Venda / Dias_Hab) END AS rep FROM(SELECT  atendimentos.polo, COUNT(*) as venda, COUNT(distinct fecha) as dias_hab, estoque.stock FROM public.atendimentos JOIN estoque ON(atendimentos.polo = estoque.polo) WHERE estoque.polo = " + "'" + cboPolos.Text + "'" + " GROUP BY atendimentos.polo, estoque.stock) as t";
-            /*
-            string strSql = `SELECT polo, stock, venda, dias_hab, 
+
+            string strSql = @"SELECT polo, stock, venda, dias_hab, 
                             ceil(venda / dias_hab) as media, 
                             ceil(stock / (venda / dias_hab)) as auto, 
                             CASE WHEN(ceil(stock / (Venda / Dias_Hab))) < 10 THEN 1 
-                                 WHEN(ceil(stock / (Venda / Dias_Hab))) >= 10 AND(ceil(stock / (Venda / Dias_Hab))) <= 13 THEN 2 
-                                 WHEN(ceil(stock / (Venda / Dias_Hab))) >= 14 AND(ceil(stock / (Venda / Dias_Hab))) <= 18 THEN 3 
-                                 WHEN(ceil(stock / (Venda / Dias_Hab))) >= 19 AND(ceil(stock / (Venda / Dias_Hab))) <= 23 THEN 4 ELSE 5 END AS cat, CASE 
-                                 WHEN(ceil(stock / (Venda / Dias_Hab))) >= 14 AND(ceil(stock / (Venda / Dias_Hab))) <= 18 THEN 0 ELSE(14 - (ceil(stock / (Venda / Dias_Hab)))) * ceil(Venda / Dias_Hab) END AS rep 
+                                 WHEN(ceil(stock / (Venda / Dias_Hab))) >= 10 
+                                  AND(ceil(stock / (Venda / Dias_Hab))) <= 13 THEN 2 
+                                 WHEN(ceil(stock / (Venda / Dias_Hab))) >= 14 
+                                  AND(ceil(stock / (Venda / Dias_Hab))) <= 18 THEN 3 
+                                 WHEN(ceil(stock / (Venda / Dias_Hab))) >= 19 
+                                  AND(ceil(stock / (Venda / Dias_Hab))) <= 23 THEN 4 
+                            ELSE 5 END AS cat, 
+                            CASE WHEN(ceil(stock / (Venda / Dias_Hab))) >= 14 
+                                  AND(ceil(stock / (Venda / Dias_Hab))) <= 18 THEN 0 
+                            ELSE(14 - (ceil(stock / (Venda / Dias_Hab)))) * ceil(Venda / Dias_Hab) END AS rep 
                             FROM(SELECT  atendimentos.polo, COUNT(*) as venda, COUNT(distinct fecha) as dias_hab, 
                                          estoque.stock FROM public.atendimentos 
                                 JOIN estoque ON(atendimentos.polo = estoque.polo) 
-                                     WHERE estoque.polo = " + "'" + cboPolos.Text + "'" + " 
-                                     GROUP BY atendimentos.polo, estoque.stock) as t`;
-            */
+                                     WHERE estoque.polo = '" + cboPolos.Text + "'"; 
+            strSql = strSql + "GROUP BY atendimentos.polo, estoque.stock) as t";
             NpgsqlCommand cmd = new NpgsqlCommand();
             cmd.CommandText = strSql;
             cmd.Connection = conex;
@@ -70,7 +73,7 @@ namespace projeto03
             tbAutosuficiencia.Text = dr[5].ToString();
             tbCategoria.Text = dr[6].ToString();
             tbReposicion.Text = dr[7].ToString();
-            dr.Close(); 
+            dr.Close();    
         }
     }
 }
